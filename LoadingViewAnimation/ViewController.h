2c6501793395a6f8f65b4dd4f385f18bf60460a9
mj230816 <songqian@sousentekiMacBook-Pro.local>
2016-01-25 11:59:55 +0800

    Revert "1.0"
    This reverts commit f7bbbbebb726faa37ef150754e9acde6971b8053.

---
+++ LoadingViewAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LoadingViewAnimation
+//
+//  Created by 宋千 on 16/1/20.
+//  Copyright © 2016年 宋千. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
